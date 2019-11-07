aa75bc74c6f48d667971ca72dbaf678d28e30415
Bhayu Kresna Pangestoe <Bhayu_Pangestoe@ikea.co.id>
2019-11-07 11:35:20 +0700

    datepicker js

--- NewDashboard/public/vendor/ckeditor/config.js
+++ NewDashboard/public/vendor/ckeditor/config.js
@@ -1,10 +1,6 @@
-/**
- * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
- */
-
 CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+	config.language = 'en';
+	config.uiColor = '#2ef05e';
+	config.height = 300;
+	config.toolbarCanCollapse = true;
 };
--- NewDashboard/webpack.mix.js
+++ NewDashboard/webpack.mix.js
@@ -17,6 +17,7 @@ mix.styles([
     'resources/vendor/animate-css/vivify.min.css',
     'resources/vendor/dropify/css/dropify.min.css',
     'resources/vendor/toastr/toastr.min.css',
+    'resources/vendor/bootstrap-datepicker/css/bootstrap-datepicker.min.css',
     'resources/assets/css/site.min.css',
     'resources/css/custom-css.css',
 ], 'public/css/all.css');
@@ -28,6 +29,7 @@ mix.scripts([
 	'resources/assets/bundles/c3.bundle.js',
 	'resources/vendor/dropify/js/dropify.min.js',
 	'resources/assets/js/pages/forms/dropify.js',
+	'resources/vendor/bootstrap-datepicker/js/bootstrap-datepicker.min.js',
 	'resources/assets/bundles/mainscripts.bundle.js',
 	'resources/assets/js/index.js',
 	'resources/js/custom-js.js',
